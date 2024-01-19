a915ee9d5165f135d90b36e4a2051514a3ab1465
giovannimegliola <giovanni.megliola@hotmail.it>
2024-01-19 11:31:00 +0100

    modified allowed origins

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -19,7 +19,7 @@ class ProjectController extends Controller
     }
 
     public function show($slug){
-        $project = Project::where('id', $slug)->with(['category', 'technologies'])->first();
+        $project = Project::where('slug', $slug)->with(['category', 'technologies'])->first();
         return response()->json(
             [
                 'success' => true,
