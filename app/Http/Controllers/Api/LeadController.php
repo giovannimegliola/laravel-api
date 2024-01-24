e29b62d96910b78d9bb32e1231dcf1b628270f04
giovannimegliola <giovanni.megliola@hotmail.it>
2024-01-24 10:54:50 +0100

    modified store function in LeadController

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewContact;
 
 class LeadController extends Controller
 {
@@ -16,20 +19,36 @@ class LeadController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+       $data = $request->all();
+       $validator = Validator::make($data, [
+           'name' => 'required',
+           'email' => 'required|email',
+           'address' => 'required',
+           'message' => 'required',
+       ]);
+
+     if ($validator->fails()) {
+         return response()->json([
+            'success' => false,
+            'errors' => $validator->errors()
+         ]);
+        }
+
+       $newLead = new Lead();
+       $newLead->fill($data);
+       $newLead->save();
+
+       Mail::to('hello@example.com')->send(new NewContact($newLead));
+
+       return response()->json([
+           'success' => true,
+       ]);
     }
 
     /**
@@ -40,21 +59,6 @@ class LeadController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Lead $lead)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Lead $lead)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
